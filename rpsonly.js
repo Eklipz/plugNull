8e8b4d0da2d1822e53e5977dfaa6a3d72ef205f5
Nate <eklipz08@gmail.com>
2016-07-16 06:45:06 -0400

    +Weed

--- rpsonly.js
+++ rpsonly.js
@@ -1289,6 +1289,41 @@ console.log(basicBot.room.name);
                     }
                 }
             },
+            
+            weedCommand: {
+                command: 'weed',
+                rank: 'user',
+                type: 'startsWith',
+                getWeeds: function (chat) {
+                    var sho = Math.floor(Math.random() * basicBot.chat.weeds.length);
+                    return basicBot.chat.weeds[sho];
+                },
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        var msg = chat.message;
+                        var space = msg.indexOf(' ');
+                        if (space === -1) {
+                            API.sendChat(basicBot.chat.eatweed);
+                            return false;
+                        }
+                        else {
+                            var name = msg.substring(space + 2);
+                            var user = basicBot.userUtilities.lookupUserName(name);
+                            if (user === false || !user.inRoom) {
+                                return API.sendChat(subChat(basicBot.chat.nouserweed, {name: name}));
+                            }
+                            else if (user.username === chat.un) {
+                                return API.sendChat(subChat(basicBot.chat.selfweed, {name: name}));
+                            }
+                            else {
+                                return API.sendChat(subChat(basicBot.chat.weed, {nameto: user.username, namefrom: chat.un, weed: this.getWeeds()}));
+                            }
+                        }
+                    }
+                }
+            },
 
             rpslsCommand: {
                 command: ['rps', 'rpsls'],
